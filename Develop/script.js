33d55dffef5a571d478ccce181e4609b2e520a8b
DAYMAN <andrew.morrow5510@gmail.com>
2020-04-15 13:00:05 -0700

    worked on javascript a little bit, added passwordLength/passwordChar

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var passwordLength = prompt("How long would you like your password to be? Pick a number between 8 and 128");
+  var passwordChar = prompt("Choose your character types:\nL - Lowercase\nU - Uppercase\nN - Numeric\nS - Special").toLowerCase();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
@@ -12,3 +15,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  
+  //set password length/complexity
+  var complexity = parseInt(passwordLength);
+
+  console.log(complexity)
+
+
+}
